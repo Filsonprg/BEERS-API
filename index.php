4112a9915a9765f4487d519bd3c4ace09ffe2197
Filsonprg <76943590+Filsonprg@users.noreply.github.com>
2023-04-13 23:31:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Seznam piv</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+		table {
+			width: 100%;
+			border-collapse: collapse;
+		}
+		th, td {
+			padding: 8px;
+			text-align: left;
+			border-bottom: 1px solid #ddd;
+			border: 1px solid;
+		}
+		tr:hover {
+			background-color: #f5f5f5;
+		}
+		tr:nth-child(even) {
+  			background-color: #D6EEEE;
+		}
+		@media screen and (max-width: 600px) {
+			table, thead, tbody, th, td, tr {
+				display: block;
+			}
+			th {
+				text-align: center;
+			}
+			td {
+				text-align: center;
+			}
+			tr {
+				margin-bottom: 20px;
+				border-bottom: 1px solid #ddd;
+			}
+			tr:last-child {
+				margin-bottom: 0;
+			}
+			tr:nth-child(even) {
+				background-color: #f2f2f2;
+			}
+			tr:nth-child(odd) {
+				background-color: #fff;
+			}
+		}
+	</style>
+</head>
+<body>
+	<h1>Seznam piv</h1>
+	<table>
+		<thead>
+			<tr>
+				<th>Značka</th>
+				<th>Název</th>
+				<th>Obsah alkoholu (%)</th>
+			</tr>
+			<?php include("beer.php"); ?>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>
+</body>
+</html>
